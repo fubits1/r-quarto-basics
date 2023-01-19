72c7715b3734673143e142bdf2b52d137e2e270c
Frie <fr1e@pm.me>
2023-01-19 19:13:02 +0100

    fix parameter things

--- 06-render.R
+++ 06-render.R
@@ -7,3 +7,12 @@ lapply(c("false", "true"), function(compress_bool) {
 })
 
 
+# compare file sizes
+out_files <- list.files("outputs", pattern = "06", full.names = TRUE)
+sizes <- file.size(out_files)
+
+sapply(seq_along(out_files), function(i) {
+  size_hr <- utils:::format.object_size(file.size(out_files[i]), "auto")
+  return(paste0(out_files[i], ": ", size_hr))
+})
+
